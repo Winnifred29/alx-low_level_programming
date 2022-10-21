bb615776b6a779293e0070818dbda3734167b2c3
Winnifred29 <113688371+Winnifred29@users.noreply.github.com>
2022-10-20 23:13:14 -0500

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,14 +5,14 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: 0
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && a >= c)
+	if (a <= b && a <= c)
 	{
 		largest = a;
 	}
@@ -25,5 +25,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	return (0);
+	return (largest);
 }
