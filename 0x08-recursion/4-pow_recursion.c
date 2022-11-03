10cb99128f3dc5a5b28ff159e7c8704df5a404bd
Winnifred29 <113688371+Winnifred29@users.noreply.github.com>
2022-11-03 14:21:25 -0500

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.c"
 
 /**
- * _pow_recursion - returns te value of x raised to the power of y.
+ * _pow_recursion - returns the value of x raised to the power of y.
  * @x: base.
  * @y: exponent.
  * REturn: value of the exponentiation.
