b391d4e6544670f6d9500f34dd5598b07b92bdb3
Winnifred29 <113688371+Winnifred29@users.noreply.github.com>
2022-11-04 07:44:55 -0500

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,8 +2,8 @@
 /**
  * main - prints the number of arguments passed into it.
  * @argc: number of command line arguments.
- * @argv: array that contains the program command line argumenst.
- * Return: 0-success.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
