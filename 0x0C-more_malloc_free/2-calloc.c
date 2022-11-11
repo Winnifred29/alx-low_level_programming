de4cd364353d4fd2fec5d4507bd606f8dfe48bd2
Mgbonye Winnifred Ebele <ebelewinnifred@gmail.com>
2022-11-11 08:52:01 -0800

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = maloc(nmemb * size); i++)
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+	
+	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
 
 	return (p);
