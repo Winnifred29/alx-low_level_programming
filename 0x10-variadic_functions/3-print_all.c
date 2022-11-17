d0bdc24a84652ed07d03ff39beef78a27618f208
Winnifred29 <113688371+Winnifred29@users.noreply.github.com>
2022-11-17 10:36:31 -0600

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -31,7 +31,7 @@ void print_all(const char * const format, ...)
 				printf("%c", va_arg(valist, int)), c = 1;
 				break;
 			case 'i':
-				printf("%d", v a_arg(valist, int)), c = 1;
+				printf("%d", va_arg(valist, int)), c = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(valist, double)), c = 1;
